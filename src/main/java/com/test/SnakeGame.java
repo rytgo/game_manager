a11a4e9822352bd81bc08a20637ac00603a1dacf
kundyzs <serzhankyzykundyz@gmail.com>
2024-11-14 22:43:12 -0800

    Setting up the initial snake

--- src/main/java/com/test/SnakeGame.java
+++ src/main/java/com/test/SnakeGame.java
@@ -2,7 +2,11 @@ package com.test;
 
 import java.util.ArrayList;
 
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
+import javafx.stage.Stage;
 
 public class SnakeGame {
     private static final int TILE_SIZE = 30;
@@ -14,4 +18,20 @@ public class SnakeGame {
     private int directionX = 1, directionY = 0; // Initial direction (right)
     private boolean gameOver = false;
     private int score = 0;
+
+    public void start(Stage primaryStage) {
+        StackPane root = new StackPane();
+        Scene scene = new Scene(root, GRID_WIDTH * TILE_SIZE, GRID_HEIGHT * TILE_SIZE);
+        
+        // Set up the initial snake
+        snake = new ArrayList<>();
+        Rectangle head = new Rectangle(TILE_SIZE, TILE_SIZE, Color.GREEN);
+        head.setX(GRID_WIDTH / 2 * TILE_SIZE);
+        head.setY(GRID_HEIGHT / 2 * TILE_SIZE);
+        snake.add(head);
+        
+        root.getChildren().addAll(snake);
+        
+        // Set up the food
+        spawnFood(root);
 }
