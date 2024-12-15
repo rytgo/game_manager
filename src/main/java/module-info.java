bcd90a610be6f920d6831bbb9cf2687bedcec0b0
Uyen Pham <uyenbao.pt@gmail.com>
2024-12-14 23:33:21 -0800

    Fix UI for Blackjack Main Menu

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module com.test {
     requires javafx.controls;
     requires javafx.graphics;
     requires java.desktop;
+    requires java.compiler;
 
     exports com.game;
     exports com.game.SnakeGame;
